f9f1d62bb054afa41b4df8da0a2c3309c5fa31a8
Ahmed Elshetany <samo7a98@mail.com>
2021-03-04 19:33:05 -0500

    created the project's structure

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {  
+    res.setHeader('Access-Control-Allow-Origin', '*');  
+    res.setHeader(    
+        'Access-Control-Allow-Headers',    
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'  
+    );  
+    res.setHeader(    
+        'Access-Control-Allow-Methods',    
+        'GET, POST, PATCH, DELETE, OPTIONS'  
+    );  
+    next();
+});
+
+app.listen(5000);
