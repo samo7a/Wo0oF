c0e637d9db2584f0ef2a83a61434c7f5fa66c5eb
Ahmed Elshetany <samo7a98@mail.com>
2021-03-12 12:40:44 -0500

    fixing deployment

--- server.js
+++ server.js
@@ -13,14 +13,20 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
-if (process.env.NODE_ENV === 'production') {  
-    // Set static folder 
-    //app.use(express.static('/frontend/build'));
-    app.use('/static', express.static(path.join(__dirname, 'frontend/build')));
-    app.get('*', (req, res) =>  {    
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));  
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(__dirname + "/frontend/build"));
+//     app.get("*", function(req, res) {
+//       res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+//     });
+// }
+// if (process.env.NODE_ENV === 'production') {  
+//     // Set static folder 
+//     //app.use(express.static('/frontend/build'));
+//     app.use('/static', express.static(path.join(__dirname, 'frontend/build')));
+//     app.get('*', (req, res) =>  {    
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));  
+//     });
+// }
 app.use('/static', express.static(path.join(__dirname, 'frontend/build')));
 app.use((req, res, next) => {  
     res.setHeader('Access-Control-Allow-Origin', '*');  
