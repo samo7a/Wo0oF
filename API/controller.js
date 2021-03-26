9b89fc1b6ea84fadd80fa7bd31f710ae04510b57
adurand2 <adurands82@gmail.com>
2021-03-26 16:11:59 -0400

    fixed issues with API files

--- API/controller.js
+++ API/controller.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 const User = require("../API/models.js");
   //User = mongoose.model('UserSchema');
-  const jwt = require('../createJWT');
+  
 
 exports.signup = function(req, res) {
+	
+  const jwt = require('../createJWT');
 	
   // incoming: login, password
   // outgoing: id, firstName, lastName, error
@@ -12,23 +14,13 @@ exports.signup = function(req, res) {
 
   const { Email, Password, Location, FirstName, LastName, userID, isOwner, ProfilePicture, ShortBio } = req.body;
 
-  const db = client.db();
 
   const newUser = new User({ Email : Email, Password: Password, Location: Location,
                              FirstName: FirstName, LastName: LastName,
                              userID: userID, isOwner: isOwner, ProfilePicture: ProfilePicture, ShortBio: ShortBio});
 
-  if (jwt.isExpired(jwtToken))
-  {
-    var r = {error:'The JWT is no longer valid'};
-    res.status(200).json(r);
-    return;
-  }
-
   try
   {
-    const db = client.db();
-    const result = db.collection('Users').insertOne(newUser);
     newUser.save();
   }
   catch(e)
