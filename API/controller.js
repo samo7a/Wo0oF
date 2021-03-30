d4a058d182a6b20e2c6b3e56fafc10c811a46622
MGo01 <matthew.gomez86@gmail.com>
2021-03-30 14:13:55 -0400

    Fixed Front-end ResetPassword by commenting it out

--- API/controller.js
+++ API/controller.js
@@ -320,3 +320,56 @@ exports.confirmPassword = function(req, res) {
 
   return res.status(200).send('Password successfully reset!');
 };
+
+// Complete confirmPassword API
+exports.reportAccounts = function(req, res) {
+
+  var { UserID, Description } = req.body;
+
+  const newReport = { Description: Description, Date: Date.now() }
+
+  // Forgive me Papa Szum for going over 100 characters
+  User.findOneAndUpdate({ _id : ObjectId(UserID) }, { $push: { SpamReports: newReport}, function(err, user)
+  {
+    // Check for any technical errors
+    if (err)
+    {
+      return res.status(500).send('Technical error while attempting to update User information.');
+    }
+    // Update JWT and send confirmation message.
+    else
+    {
+      return res.status(200).send('Report successfully inserted!');
+    }
+  });
+};
+
+// Create Dog Function
+exports.createDog = function(req, res)
+{
+  // incoming: Dog Name, password
+  // outgoing: id, firstName, lastName, error
+
+	var { Name, UserID, Bio, Breed, Weight, Height, Age, Sex } = req.body;
+
+  console.log("Json package: " + req.body + "UserID: " + UserID);
+  // Complete reportAccounts API
+
+  var DogID = DogCounter++;
+  const newDog = { DogID: DogID, Name: Name, Bio: Bio, Breed: Breed, Weight: Weight, Height: Height, Age: Age, Sex: Sex };
+
+  // Forgive me Papa Szum for going over 100 characters
+  User.findOneAndUpdate({ _id : ObjectId(UserID) }, { $push: { Dogs: newDog}}, function(err, user)
+  {
+    // Check for any technical errors
+    if (err)
+    {
+      return res.status(500).send('Technical error while attempting to update User information.');
+    }
+    // Update JWT and send confirmation message.
+    else
+    {
+      return res.status(200).send('Doge Successfully Inserted!');
+    }
+  });
+};
