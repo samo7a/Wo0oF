ebd571fdbb289485f434fe3be43c1e5fb062f7b0
MGo01 <matthew.gomez86@gmail.com>
2021-04-13 22:04:37 -0400

    Move profile picture code to route.js

--- API/ProfilePicture.js
+++ API/ProfilePicture.js
@@ -90,7 +90,7 @@ app.post('/profilePicture', upload.single('file'), (req, res) => {
 //                 err: 'No files exist'
 //             });
 //         }
-
+//
 //         // Files exist
 //         return res.json(files);
 //     });
@@ -111,29 +111,29 @@ app.post('/profilePicture', upload.single('file'), (req, res) => {
 //     });
 // });
 
-// // @route GET /image/:filename
-// // @desc Display Image
-// app.get('/image/:filename', (req, res) => {
-//     gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-//         // Check if file
-//         if (!file || file.length === 0) {
-//             return res.status(404).json({
-//                 err: 'No file exists'
-//             });
-//         }
+// @route GET /image/:filename
+// @desc Display Image
+app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        // Check if file
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        }
 
-//         // Check if image
-//         if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-//             // Read output to browser
-//             const readstream = gfs.createReadStream(file.filename);
-//             readstream.pipe(res);
-//         } else {
-//             res.status(404).json({
-//                 err: 'Not an image'
-//             });
-//         }
-//     });
-// });
+        // Check if image
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+            // Read output to browser
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        } else {
+            res.status(404).json({
+                err: 'Not an image'
+            });
+        }
+    });
+});
 
 // // @route DELETE /files/:id
 // // @desc  Delete file
--- API/routes.js
+++ API/routes.js
@@ -1,5 +1,106 @@
 module.exports = function (app) {
   var controller = require('../API/controller.js');
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const path = require('path');
+  const crypto = require('crypto');
+  const mongoose = require('mongoose');
+  const multer = require('multer');
+  const GridFsStorage = require('multer-gridfs-storage');
+  const Grid = require('gridfs-stream');
+  const methodOverride = require('method-override');
+
+  // Mongo URI
+  const mongoURI = process.env.MONGODB_URI;
+
+  // Create mongo connection
+  const conn = mongoose.createConnection(mongoURI);
+
+  // Init gfs
+  let gfs;
+
+  console.log("Routes has been invoked!");
+
+  conn.once('open', () => {
+      // Init stream
+      gfs = Grid(conn.db, mongoose.mongo);
+      gfs.collection('ProfilePictureUploads');
+  });
+
+  // Create storage engine
+  const storage = new GridFsStorage({
+      url: mongoURI,
+      file: (req, file) => {
+          return new Promise((resolve, reject) => {
+              crypto.randomBytes(16, (err, buf) => {
+                  if (err) {
+                      return reject(err);
+                  }
+                  const filename = buf.toString('hex') + path.extname(file.originalname);
+                  const fileInfo = {
+                      filename: filename,
+                      bucketName: 'ProfilePictureUploads'
+                  };
+                  resolve(fileInfo);
+              });
+          });
+      }
+  });
+  const upload = multer({ storage });
+
+  // @route GET /
+  // @desc Loads form
+  app.get('/', (req, res) => {
+      gfs.files.find().toArray((err, files) => {
+          // Check if files
+          if (!files || files.length === 0) {
+              res.render('home', { files: false });
+          } else {
+              files.map(file => {
+                  if (
+                      file.contentType === 'image/jpeg' ||
+                      file.contentType === 'image/png'
+                  ) {
+                      file.isImage = true;
+                  } else {
+                      file.isImage = false;
+                  }
+              });
+              res.render('home', { files: files });
+          }
+      });
+  });
+
+  // @route POST /upload
+  // @desc  Uploads file to DB
+  app.post('/profilePicture', upload.single('file'), (req, res) => {
+      // res.json({ file: req.file });
+      res.redirect('home');
+  });
+
+  // @route GET /image/:filename
+  // @desc Display Image
+  app.get('/image/:filename', (req, res) => {
+      gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+          // Check if file
+          if (!file || file.length === 0) {
+              return res.status(404).json({
+                  err: 'No file exists'
+              });
+          }
+
+          // Check if image
+          if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+              // Read output to browser
+              const readstream = gfs.createReadStream(file.filename);
+              readstream.pipe(res);
+          } else {
+              res.status(404).json({
+                  err: 'Not an image'
+              });
+          }
+      });
+  });
 
   // Signup route
   app.route('/signup')
