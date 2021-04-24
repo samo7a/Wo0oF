ff4484e24507f76fdf2bd89c81be49e1af431f8d
Ahmed Elshetany <samo7a98@mail.com>
2021-04-24 17:58:20 -0400

    fixing a dependency issue

--- frontend/src/images.js
+++ frontend/src/images.js
@@ -1,41 +1,42 @@
 exports.uploadFile = async (file, name) => {
+  console.log("img : " + JSON.stringify(file));
+  const AWS = require("aws-sdk");
+  AWS.config.region = "us-east-1";
 
-    const AWS = require('aws-sdk');
-    AWS.config.region = 'us-east-1';
-  
-    var albumBucketName = "wo0of";
-    var bucketRegion = "us-east-1";
-    var IdentityPoolId = "us-east-1:0a08c10f-2ff9-4818-be28-4af04d0e440a";
-  
-    AWS.config.update({
-      region: bucketRegion,
-      credentials: new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: IdentityPoolId
-      })
-    });
-  
-    var s3 = new AWS.S3({
-      apiVersion: "2006-03-01",
-      region: bucketRegion,
-      params: { Bucket: albumBucketName }
-    });
-    
+  var albumBucketName = "wo0of";
+  var bucketRegion = "us-east-1";
+  var IdentityPoolId = "us-east-1:0a08c10f-2ff9-4818-be28-4af04d0e440a";
+
+  AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: IdentityPoolId,
+    }),
+  });
+
+  var s3 = new AWS.S3({
+    apiVersion: "2006-03-01",
+    region: bucketRegion,
+    params: { Bucket: albumBucketName },
+  });
+
   var upload = new AWS.S3.ManagedUpload({
     params: {
       Bucket: albumBucketName,
       Key: name,
-      Body: file
-    }
+      Body: file,
+    },
   });
 
   var promise = upload.promise();
 
   promise.then(
-    function(data) {
+    function (data) {
       //alert("Successfully uploaded photo.");
     },
-    function(err) {
+    function (err) {
       console.log(err);
       //return alert("There was an error uploading your photo: ", err.message);
-    });
-}
+    }
+  );
+};
